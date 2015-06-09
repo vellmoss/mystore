13f8ddd2e42085b84b5654b12dfd2e5989797ec9
vellmoss <vellmoss@gmail.com>
2015-06-09 09:04:25 +0300

    добавил static_pages и изм. под них макет

--- app/models/mstore.rb
+++ app/models/mstore.rb
@@ -1,5 +1,6 @@
 class Mstore < ActiveRecord::Base
   validates_presence_of :position, :price
   validates_uniqueness_of :position
+  validates :price, numericality: {less_than: 1000, message: "Не больше 1000!"}
   self.primary_key = "id"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+
   resources :mstores
 
   # The priority is based upon order of creation: first created -> highest priority.
